aaf12c9b7a896e394c97421de23c940a89e7e466
Rafael Morais <rafael.morais@smn.com.br>
2020-06-20 08:16:33 -0300

    commit inicial

---
+++ src/mobx/index.ts
@@ -0,0 +1,17 @@
+import { router } from './router.store';
+import { home } from '../containers/home/store';
+import { combustivel } from '../containers/combustivel/store';
+import { starWars } from '../containers/star-wars/store';
+import { starWarsDetails } from '../containers/star-wars-details/store';
+import { cache } from '../containers/cache/store';
+import { tags } from '../containers/tags/store';
+
+export {
+  router,
+  home,
+  combustivel,
+  starWars,
+  starWarsDetails,
+  cache,
+  tags
+};
